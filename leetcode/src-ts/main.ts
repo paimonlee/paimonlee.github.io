48035bdb4d868930cbe5792f556b4b2e88413737
paimonlee <38132146+paimonlee@users.noreply.github.com>
2024-12-17 12:19:09 +0800

    Update main.ts

--- leetcode/src-ts/main.ts
+++ leetcode/src-ts/main.ts
@@ -1,59 +1,52 @@
 import axios from 'axios';
 import fs from 'fs';
 import path from 'node:path';
+import { Stream } from 'node:stream';
 
 async function download(link: string, threadNum: number, filename: string) {
     axios.head(link).then(async (response) => {
         if (response.status == 200 && response.headers
             && response.headers instanceof axios.AxiosHeaders) {
             const contentSize = Number(response.headers.get('content-length'));
-            const threadSize = Math.ceil(contentSize / threadNum)
-            const downloadPromises = [];
-            const arrayBuffers: Array<ArrayBuffer> = [];
+            const chunkSize = Math.ceil(contentSize / threadNum)
+            const downloadPromises: Promise<void>[] = [];
             for (let i = 0; i < threadNum; i++) {
-                const start = i * threadSize;
-                const end = Math.min((i + 1) * threadSize - 1, contentSize - 1)
-                console.log("Thread ", i, " download start:", start, " end:", end)
+                const start = i * chunkSize;
+                const end = Math.min((i + 1) * chunkSize - 1, contentSize - 1)
+                const partPath = path.join(__dirname, filename + ".part." + i)
+                const downloadSize = fs.existsSync(partPath) ? fs.statSync(partPath).size : 0;
+                const writer = fs.createWriteStream(partPath, { flags: 'a+' })
 
-                const chunkDownload = axios.get(link, {
-                    responseType: 'arraybuffer',
+                if (downloadSize >= end - start + 1) {
+                    continue;
+                }
+
+                const promise = axios.get(link, {
+                    responseType: 'stream',
                     headers: {
-                        "Range": `bytes=${start}-${end}`
+                        "Range": `bytes=${start + downloadSize}-${end}`
                     }
-                }).then(response => {
-                    arrayBuffers[i] = response.data;
+                }).then(async (response) => {
+                    response.data.pipe(writer)
+                    const waitPromise = (writer: Stream) => {
+                        return new Promise((resolve, rejects) => {
+                            writer.on('end', resolve);
+                            writer.on('error', rejects);
+                        })
+                    }
+                    await waitPromise(writer);
                 })
-                downloadPromises.push(chunkDownload)
+                downloadPromises.push(promise)
             }
             // 等待下载
             await Promise.all(downloadPromises);
-            // 计算合并后的 ArrayBuffer 的总大小
-            const totalLength = arrayBuffers.reduce((sum, buffer) => sum + buffer.byteLength, 0);
-
-            // 创建一个新的 ArrayBuffer 来存储合并后的数据
-            const mergedArrayBuffer = new ArrayBuffer(totalLength);
-
-            // 创建一个视图来操作新的 ArrayBuffer
-            let offset = 0;
-
-            // 遍历每个 ArrayBuffer，将其内容复制到新的 ArrayBuffer 中
-            arrayBuffers.forEach(buffer => {
-                // 创建当前 buffer 的 Uint8Array 视图
-                const view = new Uint8Array(buffer);
-
-                // 将当前 buffer 的数据复制到合并的 ArrayBuffer 中
-                new Uint8Array(mergedArrayBuffer, offset, view.length).set(view);
-
-                // 更新偏移量
-                offset += view.length;
-            });
-            const buffer = Buffer.from(mergedArrayBuffer);
-            const outputPath = path.join(__dirname, filename)
-            fs.openSync(outputPath, 'a+'); // 打开文件以进行写入
-            fs.writeFileSync(outputPath, buffer, { flag: 'r+' });
-            return Promise.resolve()
-        } else {
-            return Promise.reject(0)
+            const writer = fs.createWriteStream(path.join(__dirname, filename), { flags: 'a+' })
+            for (let i = 0; i < threadNum; i++) {
+                const partPath = path.join(__dirname, filename + ".part." + i)
+                const data: Buffer = fs.readFileSync(partPath)
+                console.log("read buffer size:", data.length)
+                writer.write(data);
+            }
         }
     }).catch(error => Promise.reject(error))
 }
